7e47e54b4132bc578b8aa66bde16c58a3f85d57e
Takai <tkbunbury@gmail.com>
2024-03-20 15:54:16 +0000

    Added all main routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import './App.css'
+import LoginPage from './components/LoginPage/LoginPage'
+import Leaderboard from './components/Leaderboard/Leaderboard'
+import StartScreen from './components/StartScreen/StartScreen'
+import FinalScore from './components/FinalScore/FinalScore'
 
 
 function App() {
@@ -10,8 +14,10 @@ function App() {
     <BrowserRouter>
       <>
       <Routes>
-          
-
+        <Route path='/' element={<LoginPage/>}/>
+        <Route path='/leaderboard' element={<Leaderboard/>}/>
+        <Route path='/startscreen' element={<StartScreen/>}/>
+        <Route path='/finalscore' element={<FinalScore/>}/>
       </Routes>  
       </>
     </BrowserRouter>
