db1c4231215b322adf344e6de3a43c50e682f0b4
Takai <tkbunbury@gmail.com>
2024-03-26 11:26:15 +0000

    Completed finalscore page FE

--- src/components/FinalScore/FinalScore.jsx
+++ src/components/FinalScore/FinalScore.jsx
@@ -1,5 +1,42 @@
-function FinalScore(){
+import { useNavigate } from 'react-router-dom';
+import './FinalScore.css';
 
+function FinalScore({ score, isNewHighScore, leaderboardPosition }) {
+    const navigate = useNavigate();
+    score = 99999
+    leaderboardPosition = 1
+
+    const handleFlyAgainClick = () => {
+        // Logic to start the game again
+    };
+
+    const handleLeaderboardClick = () => {
+        navigate('/leaderboard');
+    };
+
+    const handleLogout = () => {
+        // We need to add Logic to logout the user and clear user data from local storage)
+        navigate('/');
+    };
+
+    return (
+        <div className="final-score-page">
+            <h1 className="score-header">
+                {isNewHighScore ? 'NEW HIGH SCORE!' : 'Your Final Score'}
+            </h1>
+            <p className="score">{score}</p>
+            <p className="position">
+                {isNewHighScore ? 'Congratulations! You made it to position ' : 'Your position on the leaderboard is '}
+                {leaderboardPosition}
+            </p>
+            
+            <button className="fly-again-button" onClick={handleFlyAgainClick}>Fly Again</button>
+            <button className="logout-button" onClick={handleLogout}>Logout</button>
+            <button className="leaderboard-button" onClick={handleLeaderboardClick}>
+                Go to Leaderboard
+            </button>
+        </div>
+    );
 }
 
-export default FinalScore
+export default FinalScore;
